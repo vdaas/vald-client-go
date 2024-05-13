a2da8933adce2c4d73b761f89dbc499a6371325e
Vdaas CI <vald@vdaas.org>
2024-05-13 03:48:29 +0000

    Update [vald sha: 6d4b50543c6a900594aabca3a44b160fd27964d9]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- go.mod
+++ go.mod
@@ -3,42 +3,40 @@ module github.com/vdaas/vald-client-go
 go 1.21
 
 replace (
-	cloud.google.com/go => cloud.google.com/go v0.112.1-0.20240221201021-e3fecfd3f4e6
-	cloud.google.com/go/bigquery => cloud.google.com/go/bigquery v1.59.2-0.20240221201021-e3fecfd3f4e6
+	cloud.google.com/go => cloud.google.com/go v0.113.1-0.20240510210617-7c529786275a
+	cloud.google.com/go/bigquery => cloud.google.com/go/bigquery v1.61.1-0.20240510210617-7c529786275a
 	github.com/envoyproxy/protoc-gen-validate => github.com/envoyproxy/protoc-gen-validate v1.0.4
-	github.com/golang/protobuf => github.com/golang/protobuf v1.5.3
+	github.com/golang/protobuf => github.com/golang/protobuf v1.5.4
 	github.com/google/go-cmp => github.com/google/go-cmp v0.6.0
-	golang.org/x/crypto => golang.org/x/crypto v0.19.0
+	golang.org/x/crypto => golang.org/x/crypto v0.23.0
 	golang.org/x/lint => golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
-	golang.org/x/mod => golang.org/x/mod v0.15.0
-	golang.org/x/net => golang.org/x/net v0.21.0
-	golang.org/x/oauth2 => golang.org/x/oauth2 v0.17.0
-	golang.org/x/sync => golang.org/x/sync v0.6.0
-	golang.org/x/sys => golang.org/x/sys v0.17.0
-	golang.org/x/text => golang.org/x/text v0.14.0
-	golang.org/x/tools => golang.org/x/tools v0.18.0
+	golang.org/x/mod => golang.org/x/mod v0.17.0
+	golang.org/x/net => golang.org/x/net v0.25.0
+	golang.org/x/oauth2 => golang.org/x/oauth2 v0.20.0
+	golang.org/x/sync => golang.org/x/sync v0.7.0
+	golang.org/x/sys => golang.org/x/sys v0.20.0
+	golang.org/x/text => golang.org/x/text v0.15.0
+	golang.org/x/tools => golang.org/x/tools v0.21.0
 	golang.org/x/xerrors => golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028
 	google.golang.org/appengine => google.golang.org/appengine v1.6.8
-	google.golang.org/genproto => google.golang.org/genproto v0.0.0-20240221002015-b0ce06bbee7c
-	google.golang.org/genproto/googleapis/api => google.golang.org/genproto/googleapis/api v0.0.0-20240221002015-b0ce06bbee7c
-	google.golang.org/genproto/googleapis/rpc => google.golang.org/genproto/googleapis/rpc v0.0.0-20240221002015-b0ce06bbee7c
-	google.golang.org/grpc => google.golang.org/grpc v1.62.0
-	google.golang.org/protobuf => google.golang.org/protobuf v1.32.0
+	google.golang.org/genproto => google.golang.org/genproto v0.0.0-20240509183442-62759503f434
+	google.golang.org/genproto/googleapis/api => google.golang.org/genproto/googleapis/api v0.0.0-20240509183442-62759503f434
+	google.golang.org/genproto/googleapis/rpc => google.golang.org/genproto/googleapis/rpc v0.0.0-20240509183442-62759503f434
+	google.golang.org/grpc => google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf => google.golang.org/protobuf v1.34.1
 )
 
 require (
-	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.32.0-20240221180331-f05a6f4403ce.1
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.1-20240508200655-46a4cf4ba109.1
 	github.com/planetscale/vtprotobuf v0.6.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20240213162025-012b6fc9bca9
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240213162025-012b6fc9bca9
-	google.golang.org/grpc v1.61.1
-	google.golang.org/protobuf v1.32.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240429193739-8cf5692501f6
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.34.1
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
--- v1/payload/payload_vtproto.pb.go
+++ v1/payload/payload_vtproto.pb.go
@@ -22,10 +22,12 @@ import (
 	math "math"
 
 	protohelpers "github.com/planetscale/vtprotobuf/protohelpers"
+	wrapperspb1 "github.com/planetscale/vtprotobuf/types/known/wrapperspb"
 	io "io"
 	status "google.golang.org/genproto/googleapis/rpc/status"
 	proto "google.golang.org/protobuf/proto"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 const (
@@ -181,6 +183,7 @@ func (m *Search_Config) CloneVT() *Search_Config {
 	r.EgressFilters = m.EgressFilters.CloneVT()
 	r.MinNum = m.MinNum
 	r.AggregationAlgorithm = m.AggregationAlgorithm
+	r.Ratio = (*wrapperspb.FloatValue)((*wrapperspb1.FloatValue)(m.Ratio).CloneVT())
 	if len(m.unknownFields) > 0 {
 		r.unknownFields = make([]byte, len(m.unknownFields))
 		copy(r.unknownFields, m.unknownFields)
@@ -826,6 +829,38 @@ func (m *Remove) CloneVT() *Remove {
 }
 
 func (m *Remove) CloneMessageVT() proto.Message {
+	return m.CloneVT()
+}
+
+func (m *Flush_Request) CloneVT() *Flush_Request {
+	if m == nil {
+		return (*Flush_Request)(nil)
+	}
+	r := new(Flush_Request)
+	if len(m.unknownFields) > 0 {
+		r.unknownFields = make([]byte, len(m.unknownFields))
+		copy(r.unknownFields, m.unknownFields)
+	}
+	return r
+}
+
+func (m *Flush_Request) CloneMessageVT() proto.Message {
+	return m.CloneVT()
+}
+
+func (m *Flush) CloneVT() *Flush {
+	if m == nil {
+		return (*Flush)(nil)
+	}
+	r := new(Flush)
+	if len(m.unknownFields) > 0 {
+		r.unknownFields = make([]byte, len(m.unknownFields))
+		copy(r.unknownFields, m.unknownFields)
+	}
+	return r
+}
+
+func (m *Flush) CloneMessageVT() proto.Message {
 	return m.CloneVT()
 }
 
@@ -2070,6 +2105,9 @@ func (this *Search_Config) EqualVT(that *Search_Config) bool {
 	if this.AggregationAlgorithm != that.AggregationAlgorithm {
 		return false
 	}
+	if !(*wrapperspb1.FloatValue)(this.Ratio).EqualVT((*wrapperspb1.FloatValue)(that.Ratio)) {
+		return false
+	}
 	return string(this.unknownFields) == string(that.unknownFields)
 }
 
@@ -2929,6 +2967,38 @@ func (this *Remove) EqualVT(that *Remove) bool {
 
 func (this *Remove) EqualMessageVT(thatMsg proto.Message) bool {
 	that, ok := thatMsg.(*Remove)
+	if !ok {
+		return false
+	}
+	return this.EqualVT(that)
+}
+func (this *Flush_Request) EqualVT(that *Flush_Request) bool {
+	if this == that {
+		return true
+	} else if this == nil || that == nil {
+		return false
+	}
+	return string(this.unknownFields) == string(that.unknownFields)
+}
+
+func (this *Flush_Request) EqualMessageVT(thatMsg proto.Message) bool {
+	that, ok := thatMsg.(*Flush_Request)
+	if !ok {
+		return false
+	}
+	return this.EqualVT(that)
+}
+func (this *Flush) EqualVT(that *Flush) bool {
+	if this == that {
+		return true
+	} else if this == nil || that == nil {
+		return false
+	}
+	return string(this.unknownFields) == string(that.unknownFields)
+}
+
+func (this *Flush) EqualMessageVT(thatMsg proto.Message) bool {
+	that, ok := thatMsg.(*Flush)
 	if !ok {
 		return false
 	}
@@ -4681,6 +4751,16 @@ func (m *Search_Config) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.Ratio != nil {
+		size, err := (*wrapperspb1.FloatValue)(m.Ratio).MarshalToSizedBufferVT(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(size))
+		i--
+		dAtA[i] = 0x52
+	}
 	if m.AggregationAlgorithm != 0 {
 		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.AggregationAlgorithm))
 		i--
@@ -6252,6 +6332,72 @@ func (m *Remove) MarshalToVT(dAtA []byte) (int, error) {
 }
 
 func (m *Remove) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
+	if m == nil {
+		return 0, nil
+	}
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.unknownFields != nil {
+		i -= len(m.unknownFields)
+		copy(dAtA[i:], m.unknownFields)
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *Flush_Request) MarshalVT() (dAtA []byte, err error) {
+	if m == nil {
+		return nil, nil
+	}
+	size := m.SizeVT()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBufferVT(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *Flush_Request) MarshalToVT(dAtA []byte) (int, error) {
+	size := m.SizeVT()
+	return m.MarshalToSizedBufferVT(dAtA[:size])
+}
+
+func (m *Flush_Request) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
+	if m == nil {
+		return 0, nil
+	}
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.unknownFields != nil {
+		i -= len(m.unknownFields)
+		copy(dAtA[i:], m.unknownFields)
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *Flush) MarshalVT() (dAtA []byte, err error) {
+	if m == nil {
+		return nil, nil
+	}
+	size := m.SizeVT()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBufferVT(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *Flush) MarshalToVT(dAtA []byte) (int, error) {
+	size := m.SizeVT()
+	return m.MarshalToSizedBufferVT(dAtA[:size])
+}
+
+func (m *Flush) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 	if m == nil {
 		return 0, nil
 	}
@@ -8791,6 +8937,10 @@ func (m *Search_Config) SizeVT() (n int) {
 	if m.AggregationAlgorithm != 0 {
 		n += 1 + protohelpers.SizeOfVarint(uint64(m.AggregationAlgorithm))
 	}
+	if m.Ratio != nil {
+		l = (*wrapperspb1.FloatValue)(m.Ratio).SizeVT()
+		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
+	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -9322,6 +9472,26 @@ func (m *Remove_Config) SizeVT() (n int) {
 }
 
 func (m *Remove) SizeVT() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	n += len(m.unknownFields)
+	return n
+}
+
+func (m *Flush_Request) SizeVT() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	n += len(m.unknownFields)
+	return n
+}
+
+func (m *Flush) SizeVT() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -11143,6 +11313,42 @@ func (m *Search_Config) UnmarshalVT(dAtA []byte) error {
 					break
 				}
 			}
+		case 10:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Ratio", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Ratio == nil {
+				m.Ratio = &wrapperspb.FloatValue{}
+			}
+			if err := (*wrapperspb1.FloatValue)(m.Ratio).UnmarshalVT(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
@@ -14231,6 +14437,108 @@ func (m *Remove) UnmarshalVT(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: Remove: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.unknownFields = append(m.unknownFields, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *Flush_Request) UnmarshalVT(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return protohelpers.ErrIntOverflow
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: Flush_Request: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: Flush_Request: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.unknownFields = append(m.unknownFields, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *Flush) UnmarshalVT(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return protohelpers.ErrIntOverflow
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: Flush: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: Flush: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
